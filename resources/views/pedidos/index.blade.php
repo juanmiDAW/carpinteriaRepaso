f032c5b2edcfee19bcb1686bf10a960833e76ce9
DAW <juanmiguel.valle@iesdonana.org>
2025-06-05 13:07:15 +0200

    se quitan boton en index de pedidos

--- resources/views/pedidos/index.blade.php
+++ resources/views/pedidos/index.blade.php
@@ -68,12 +68,7 @@
                                 </tbody>
                             </table>
                         </div>
-                        <div class="mt-6 text-center">
-                            <a href="{{ route('muebles.create') }}"
-                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none">
-                                Crear un nuevo mueble
-                            </a>
-                        </div>
+                    
                     </div>
                 </div>
             </div>
